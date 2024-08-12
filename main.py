dc9694a981d7f9bec94819bc1ef184bc87fd00b3
nata-rozanova80 <nata.rozanova80@gmail.com>
2024-08-12 21:48:39 +0300

    2 new codes

--- main.py
+++ main.py
@@ -5,29 +5,26 @@ def guess_the_number():
     attempts = 0
     guessed = False
 
-    print("ЧЕГОТОТАМЧЕГОТОТАМДобро пожаловать в игру 'Угадай число'!")
-    print("ТРАЛЯЛЯЛЯЛЯЯ загадал число от 1 до 100. Попробуйте угадать его!")
-    a = 0
+    print("Приветствуем вас в игре 'Угадай число'!")
+    print("Я загадал число от 1 до 100. Удачи!")
 
     while not guessed:
         player_guess = input("Введите ваше предположение: ")
 
-        # Проверяем, является ли ввод числом
         if not player_guess.isdigit():
             print("Пожалуйста, введите число.")
             continue
 
         player_guess = int(player_guess)
-        a +=1
+        attempts += 1
 
-        print ("конфликт")
         if player_guess < number_to_guess:
-            print(a)
+            print("Слишком низко! Попробуйте еще раз.")
         elif player_guess > number_to_guess:
             print("Слишком высоко! Попробуйте еще раз.")
-        else:
+            else:
             guessed = True
-            print(f"Поздравляю! Вы угадали число {number_to_guess} за {attempts} попыток.")
+            print(f"Вы сделали это! Число было {number_to_guess}, и вы угадали его за {attempts} попыток!")
 
-if __name__ == "__main__":
-    guess_the_number()
+        if __name__ == "__main__":  # Обратите внимание на правильное использование __name__
+            guess_the_number()
